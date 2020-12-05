5ea5f3e2f502144b0132763fc30c3b03352ae42b
JulesGum <juls.gumenna@gmail.com>
2020-12-05 23:03:00 +0200

    add less3

---
+++ node/hw3/dataBase/users.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        name: 'Jules', surname: 'Gum', email: 'jules@gmail.com', password: '123456'
+    },
+    {
+        name: 'Stef', surname: 'Kushyk', email: 'stef@gmail.com', password: '456789'
+    }
+];
